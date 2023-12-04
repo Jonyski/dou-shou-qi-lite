f6e105a3acd483bdad2561c021df4bff972694a9
jonyski <geleiaeavela@gmail.com>
2023-12-04 12:19:52 -0300

    initializes the vue project and creates initial components

---
+++ src/components/GameBoard.vue
@@ -0,0 +1,327 @@
+<script setup>
+	import Piece from './Piece.vue'
+</script>
+
+<template>
+	<section id="board">
+		<div class="row">
+			<div class="square normal" data-square-type="normal">
+				<Piece :piece-value="7" img-source="./lion.jpg"/>
+			</div>
+
+			<div class="square normal" data-square-type="normal">
+				
+			</div>
+
+			<!-- trap is a square in witch the pieces ranking don't matter, for example a cat can eat an elephant -->
+			<div class="square trap" data-square-type="trap">
+				
+			</div>
+
+			<!-- den is the square in witch the opponent has to get to in order to win -->
+			<div class="square den" data-square-type="player2-den">
+				
+			</div>
+
+			<div class="square trap" data-square-type="trap">
+				
+			</div>
+
+			<div class="square normal" data-square-type="normal">
+				
+			</div>
+
+			<div class="square normal" data-square-type="normal">
+				<Piece :piece-value="6" img-source="./tiger.jpg"/>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="square normal" data-square-type="normal">
+				
+			</div>
+
+			<div class="square normal" data-square-type="normal">
+				<Piece :piece-value="3" img-source="./pig.jpg"/>
+			</div>
+
+			<div class="square normal" data-square-type="normal">
+				
+			</div>
+
+			<div class="square trap" data-square-type="trap">
+				
+			</div>
+
+			<div class="square normal" data-square-type="normal">
+				
+			</div>
+
+			<div class="square normal" data-square-type="normal">
+				<Piece :piece-value="2" img-source="./cat.jpg"/>
+			</div>
+
+			<div class="square normal" data-square-type="normal">
+				
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="square normal" data-square-type="normal">
+				<Piece :piece-value="1" img-source="./rat.jpg"/>
+			</div>
+
+			<div class="square normal" data-square-type="normal">
+				
+			</div>
+
+			<div class="square normal" data-square-type="normal">
+				<Piece :piece-value="5" img-source="./monkey.jpg"/>
+			</div>
+
+			<div class="square normal" data-square-type="normal">
+				
+			</div>
+
+			<div class="square normal" data-square-type="normal">
+				<Piece :piece-value="4" img-source="./bear.jpg"/>
+			</div>
+
+			<div class="square normal" data-square-type="normal">
+				
+			</div>
+
+			<div class="square normal" data-square-type="normal">
+				<Piece :piece-value="8" img-source="./elephant.jpg"/>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="square normal" data-square-type="normal">
+				
+			</div>
+
+			<div class="square river" data-square-type="river">
+				
+			</div>
+
+			<div class="square river" data-square-type="river">
+				
+			</div>
+
+			<div class="square normal" data-square-type="normal">
+				
+			</div>
+
+			<div class="square river" data-square-type="river">
+				
+			</div>
+
+			<div class="square river" data-square-type="river">
+				
+			</div>
+
+			<div class="square normal" data-square-type="normal">
+				
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="square normal" data-square-type="normal">
+				
+			</div>
+
+			<div class="square river" data-square-type="river">
+				
+			</div>
+
+			<div class="square river" data-square-type="river">
+				
+			</div>
+
+			<div class="square normal" data-square-type="normal">
+				
+			</div>
+
+			<div class="square river" data-square-type="river">
+				
+			</div>
+
+			<div class="square river" data-square-type="river">
+				
+			</div>
+
+			<div class="square normal" data-square-type="normal">
+				
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="square normal" data-square-type="normal">
+				
+			</div>
+
+			<div class="square river" data-square-type="river">
+				
+			</div>
+
+			<div class="square river" data-square-type="river">
+				
+			</div>
+
+			<div class="square normal" data-square-type="normal">
+				
+			</div>
+
+			<div class="square river" data-square-type="river">
+				
+			</div>
+
+			<div class="square river" data-square-type="river">
+				
+			</div>
+
+			<div class="square normal" data-square-type="normal">
+				
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="square normal" data-square-type="normal">
+				<Piece :piece-value="8" img-source="./elephant.jpg"/>
+			</div>
+
+			<div class="square normal" data-square-type="normal">
+				
+			</div>
+
+			<div class="square normal" data-square-type="normal">
+				<Piece :piece-value="4" img-source="./bear.jpg"/>
+			</div>
+
+			<div class="square normal" data-square-type="normal">
+				
+			</div>
+
+			<div class="square normal" data-square-type="normal">
+				<Piece :piece-value="5" img-source="./monkey.jpg"/>
+			</div>
+
+			<div class="square normal" data-square-type="normal">
+				
+			</div>
+
+			<div class="square normal" data-square-type="normal">
+				<Piece :piece-value="rat" img-source="./rat.jpg"/>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="square normal" data-square-type="normal">
+				
+			</div>
+
+			<div class="square normal" data-square-type="normal">
+				<Piece :piece-value="2" img-source="./cat.jpg"/>
+			</div>
+
+			<div class="square normal" data-square-type="normal">
+				
+			</div>
+
+			<div class="square trap" data-square-type="trap">
+				
+			</div>
+
+			<div class="square normal" data-square-type="normal">
+				
+			</div>
+
+			<div class="square normal" data-square-type="normal">
+				<Piece :piece-value="3" img-source="./pig.jpg"/>
+			</div>
+
+			<div class="square normal" data-square-type="normal">
+				
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="square normal" data-square-type="normal">
+				<Piece :piece-value="6" img-source="./tiger.jpg"/>
+			</div>
+
+			<div class="square normal" data-square-type="normal">
+				
+			</div>
+
+			<div class="square trap" data-square-type="trap">
+				
+			</div>
+
+			<div class="square den" data-square-type="player1-den">
+				
+			</div>
+
+			<div class="square trap" data-square-type="trap">
+				
+			</div>
+
+			<div class="square normal" data-square-type="normal">
+				
+			</div>
+
+			<div class="square normal" data-square-type="normal">
+				<Piece :piece-value="7" img-source="./lion.jpg"/>
+			</div>
+		</div>
+	</section>
+</template>
+
+<style scoped>
+	#board{
+		height: 81vh;
+		width: 63vh;
+		background-image: url("/dou-shou-qi_board.jpg");
+		background-size: cover;
+		background-position-x: -3px;
+		background-position-y: -2px;
+		background-repeat: no-repeat;
+
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-start;
+	}
+
+	.row{
+		display: flex;
+		flex-direction: row;
+		justify-content: flex-start;
+	}
+
+	.square{
+		aspect-ratio: 1/1;
+		height: 9vh;
+		width: 9vh;
+
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	/*.trap{
+		background-color: rgba(255, 50, 80, 0.1);
+	}
+	.den{
+		background-color: rgba(255, 0, 0, 0.1);
+	}
+	.river{
+		background-color: rgba(0, 0, 255, 0.1);
+	}
+	.normal{
+		background-color: rgba(255, 255, 255, 0.1);
+	}*/
+	.square:hover{
+		background-color: rgba(255, 255, 255, 0.3);
+		transition-duration: 0.2s;
+	}
+</style>
---
+++ src/components/Piece.vue
@@ -0,0 +1,28 @@
+<script setup>
+	defineProps({
+		pieceValue: Number,
+		imgSource: String
+	})
+</script>
+
+<template>
+	<div class="piece-div" :data-pieceValue="pieceValue">
+		<img class="piece-img" :src="imgSource" draggable="false">
+	</div>
+</template>
+
+<style scoped>
+	.piece-div{
+		aspect-ratio: 1/1;
+		border-radius: 50%;
+		width: 8vh;
+		height: 8vh;
+		align-self: center;
+		justify-self: center;
+	}
+	.piece-img{
+		width: 100%;
+		height: 100%;
+		border-radius: 50%;
+	}
+</style>
