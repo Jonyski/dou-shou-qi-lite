f36d0f9c3b312e40b7bf0dc669a463324b8b1693
jonyski <geleiaeavela@gmail.com>
2023-12-06 23:21:11 -0300

    adds cemeteries to the game

---
+++ src/components/Cemetery.vue
@@ -0,0 +1,23 @@
+<script setup>
+	const props = defineProps({
+		playerNumber: Number
+	})
+</script>
+
+<template>
+	<section class="cemetery" :id="'P' + playerNumber + 'cemetery'">
+		
+	</section>
+</template>
+
+<style scoped>
+	.cemetery{
+		height: 80vh;
+		width: 16vh;
+
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-start;
+		align-items: center;
+	}
+</style>
